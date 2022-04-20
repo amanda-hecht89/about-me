30b12febd3f0997ce2ac60d3ba02609b617d7559
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-04-20 16:53:45 -0600

    basics

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // import functions
 
 // grab DOM elements
-
+const showButton = document.getElementById('anibutton');
+const animal = document.getElementById('animalDev');
+console.log(showButton, animal);
 // set event listeners 
+showButton.addEventListener('click', ()=> {
+    animal.classList.remove('hidden');
+});
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
